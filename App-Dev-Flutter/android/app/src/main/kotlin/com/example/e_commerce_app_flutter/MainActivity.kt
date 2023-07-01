a58f5a6102bbfbc494d8e318174b84633adfb88b
shrijanani-sj <shrijanani0606@gmail.com>
2023-07-01 12:24:08 +0530

    taks 1

---
+++ App-Dev-Flutter/android/app/src/main/kotlin/com/example/e_commerce_app_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.clothshoppinguser
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ App-Dev-Flutter/lib/services/data_streams/category_products_stream.dart
@@ -0,0 +1,19 @@
+import 'package:clothshoppinguser/models/Product.dart';
+import 'package:clothshoppinguser/services/data_streams/data_stream.dart';
+import 'package:clothshoppinguser/services/database/product_database_helper.dart';
+
+class CategoryProductsStream extends DataStream<List<String>> {
+  final ProductType category;
+
+  CategoryProductsStream(this.category);
+  @override
+  void reload() {
+    final allProductsFuture =
+        ProductDatabaseHelper().getCategoryProductsList(category);
+    allProductsFuture.then((favProducts) {
+      addData(favProducts);
+    }).catchError((e) {
+      addError(e);
+    });
+  }
+}
